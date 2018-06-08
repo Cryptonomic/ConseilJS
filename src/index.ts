e67fa12cd67a76c333efc97eb21d65f344b13486
Vishakh <vishakh@users.noreply.github.com>
2018-06-08 01:54:22 -0400

    Set up Webpack

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+module.exports = Object.assign(
+    {},
+    require('./tezos/TezosQuery'),
+    require('./tezos/TezosOperations'),
+    require('./tezos/TezosWallet'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.ts",
+    output: {
+        filename: "bundle.js"
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    node: {
+        // handle "Can't resolve 'fs'" issue
+        fs: 'empty'
+    }
+};
