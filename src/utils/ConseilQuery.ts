e09ac67a011033c382882579bda024247b91e8d7
Vishakh <vishakh@users.noreply.github.com>
2018-06-23 23:02:34 -0400

    Added support for custom overrides for Conseil queries.

--- src/utils/ConseilQuery.ts
+++ src/utils/ConseilQuery.ts
@@ -1,23 +1,35 @@
 import * as querystring from "querystring";
 import fetch from 'node-fetch';
-import {TezosFilter} from "../tezos/TezosConseilQuery";
+import {TezosFilter} from "..";
 
 /**
  * Utility functions for querying backend Conseil API
  */
 
 /**
- * Runs a query against Conseil backend API
+ * Get Conseil URL to query
  * TODO: Remove hard coded URL
+ * @param {string} network  Network to go against
+ * @param {string} route API route to query
+ * @param {string} customUrl    Custom URL override
+ * @returns {string}   The URL
+ */
+export function getConseilURL(network: string, route: string, customUrl = '') {
+    if(customUrl != '') return `${customUrl}/${route}`;
+    else return `http://206.189.230.137:1337/tezos/${network}/${route}`;
+}
+
+/**
+ * Runs a query against Conseil backend API
  * TODO: Also make the blockchain a parameter
  * @param {string} network  Network to go against
  * @param {string} route API route to query
- * @param {string} payload  Body of query
+ * @param customUrl
  * @returns {Promise<object>}   JSON representation of response from Conseil
  */
-export function queryConseilServer(network: string, route: string): Promise<object> {
-    let url = `http://206.189.230.137:1337/tezos/${network}/${route}`;
-    console.log(`Querying Conseil server at URL ${url}`)
+export function queryConseilServer(network: string, route: string, customUrl = ''): Promise<object> {
+    const url = getConseilURL(network, route, customUrl);
+    console.log(`Querying Conseil server at URL ${url}`);
     return fetch(url, {
         method: 'get',
         headers: {
@@ -32,12 +44,13 @@ export function queryConseilServer(network: string, route: string): Promise<obje
  * @param {string} network  Network to go against
  * @param {string} route    API route to query
  * @param {TezosFilter} filter  Conseil filter
+ * @param {string} customUrl Custom URL override
  * @returns {Promise<object>}   Data returned by Conseil as a JSON object
  */
-export function queryConseilServerWithFilter(network: string, route: string, filter: TezosFilter): Promise<object> {
+export function queryConseilServerWithFilter(network: string, route: string, filter: TezosFilter, customUrl = ''): Promise<object> {
     let params = querystring.stringify(sanitizeFilter(filter));
     let cmdWithParams = `${route}?${params}`;
-    return queryConseilServer(network, cmdWithParams);
+    return queryConseilServer(network, cmdWithParams, customUrl);
 }
 
 /**
