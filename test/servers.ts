e8e4fe33ba97154a19c97af18a7ff6767fac3853
developer0623 <developer0623@yahoo.com>
2018-10-02 14:11:05 -0500

    fixed `npm run build`

---
+++ test/servers.ts
@@ -0,0 +1,6 @@
+export const servers = {
+    conseilServer: 'https://conseil-staging.cryptonomic-infra.tech/tezos/zeronet',
+    conseilApiKey: 'hooman',
+    tezosServer: 'https://tezos-staging.cryptonomic-infra.tech/'
+}
+ 
