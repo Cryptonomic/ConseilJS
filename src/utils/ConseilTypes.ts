bbd416e60bf96038d83088a67538c5745fc3dd3b
Basil <ba205@users.noreply.github.com>
2018-10-30 18:40:25 -0400

    Changed manager_pub_key to managerPubKey in ConseilTypes.ts to make develop compatible with alphanet over zeronet

--- src/utils/ConseilTypes.ts
+++ src/utils/ConseilTypes.ts
@@ -43,7 +43,7 @@ export interface TezosOperation {
     source: string,
     amount: string,
     destination: string,
-    manager_pub_key: string,
+    managerPubKey: string,
     balance: string,
     delegate: string,
     operationGroupHash: string,
