8b5b68e753e240a7410189991af7f27d868f8a27
anonymoussprocket <anonymoussprocket@gmail.com>
2018-12-29 21:20:15 -0500

    - coverage build target

---
+++ test/servers.ts
@@ -0,0 +1,5 @@
+export const servers = {
+ conseilServer: 'https://conseil-staging.cryptonomic-infra.tech/tezos/alphanet',
+ conseilApiKey: 'hooman',
+ tezosServer: 'https://tezos-staging.cryptonomic-infra.tech/'
+}
