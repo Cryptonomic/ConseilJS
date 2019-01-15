8a8a5c4bb6d8fa278aa55f606a8f0f2c7257acc5
Sean M <sean@arrowburn.co.nz>
2019-01-15 16:39:36 +1300

    added invoke and originate functions to ConseilJS based on develop branch

--- src/tezos/TezosTypes.ts
+++ src/tezos/TezosTypes.ts
@@ -3,82 +3,121 @@
  */
 
 export interface BlockHeader {
-    level: number,
-    proto: number,
-    predecessor: string,
-    timestamp: string,
-    validation_pass: number,
-    operations_hash: string,
-    fitness: string[],
-    context: string,
-    priority: number,
-    proof_of_work_nonce: string,
-    signature: string
+  level: number;
+  proto: number;
+  predecessor: string;
+  timestamp: string;
+  validation_pass: number;
+  operations_hash: string;
+  fitness: string[];
+  context: string;
+  priority: number;
+  proof_of_work_nonce: string;
+  signature: string;
 }
 
 export interface BlockMetadata {
-    protocol: string,
-    chain_id: string,
-    hash: string,
-    metadata: BlockHeader
+  protocol: string;
+  chain_id: string;
+  hash: string;
+  metadata: BlockHeader;
 }
 
 export interface AccountDelegate {
-    setable: boolean,
-    value: string
+  setable: boolean;
+  value: string;
 }
 
 export interface Account {
-    manager: string,
-    balance: number,
-    spendable: boolean,
-    delegate: AccountDelegate,
-    script: string,
-    counter: number
+  manager: string;
+  balance: number;
+  spendable: boolean;
+  delegate: AccountDelegate;
+  script: string;
+  counter: number;
 }
 
 export interface ManagerKey {
-    manager: string,
-    key: string
+  manager: string;
+  key: string;
 }
 
 export interface AlphaOperationResult {
-    status: string,
-    originated_contracts: string[]
-    errors: string[]
+  status: string;
+  originated_contracts: string[];
+  errors: string[];
 }
 
 export interface AlphaOperationContentsAndResult {
-    kind: string,
-    metadata: AlphaOperationResult
+  kind: string;
+  metadata: AlphaOperationResult;
 }
 
 export interface AlphaOperationsWithMetadata {
-    contents: AlphaOperationContentsAndResult[],
-    signature: string,
-    kind: string, //only if error
-    id: string, //only if error
-    contract: string //only if error
+  contents: AlphaOperationContentsAndResult[];
+  signature: string;
+  kind: string; //only if error
+  id: string; //only if error
+  contract: string; //only if error
 }
 
 export interface InjectedOperation {
-    injectedOperation: string
+  injectedOperation: string;
 }
 
 // abstracts Transaction, Delegations, and Originations
 export interface Operation {
-    kind: string,
-    source: string,
-    fee: string,
-    counter: string,
-    storage_limit: string,
-    gas_limit: string,
-    delegate?: String,
-    public_key?: string,
-    managerPubkey?: string, //initially manager_pubkey
-    balance?: string,
-    spendable?: boolean,
-    delegatable?: boolean,
-    destination?: String,
-    amount?: string
+  kind: string;
+  source: string;
+  fee: string;
+  counter: string;
+  storage_limit: string;
+  gas_limit: string;
+  delegate?: String;
+  public_key?: string;
+  managerPubkey?: string; // mainnet, alphanet
+  //manager_pubkey?: string, // zeronet
+  balance?: string;
+  spendable?: boolean;
+  delegatable?: boolean;
+  destination?: String;
+  amount?: string;
+}
+
+export interface ContractOriginationOperation {
+  script: object;
+  kind: string;
+  source: string;
+  fee: string;
+  counter: string;
+  storage_limit: string;
+  gas_limit: string;
+  delegate?: String;
+  public_key?: string;
+  //   managerPubkey?: string; // mainnet, alphanet
+  manager_pubkey?: string; // zeronet
+  balance?: string;
+  spendable?: boolean;
+  delegatable?: boolean;
+  destination?: String;
+  amount?: string;
+}
+
+export interface ContractInvocationOperation {
+  kind: string;
+  source: string;
+  fee: string;
+  counter: string;
+  storage_limit: string;
+  gas_limit: string;
+  delegate?: String;
+  public_key?: string;
+  parameters: object;
+  //   managerPubkey?: string; // mainnet, alphanet
+  manager_pubkey?: string; // zeronet
+  balance?: string;
+  spendable?: boolean;
+  delegatable?: boolean;
+  destination?: String;
+  amount?: string;
 }
