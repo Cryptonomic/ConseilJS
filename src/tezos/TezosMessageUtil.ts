82d8d2151661ac6e7675717f7c2606a4fd06a2de
anonymoussprocket <anonymoussprocket@gmail.com>
2019-01-14 21:35:02 -0500

    - includes cleanup

--- src/tezos/TezosMessageUtil.ts
+++ src/tezos/TezosMessageUtil.ts
@@ -10,7 +10,7 @@ export namespace TezosMessageUtils {
    * Encodes an integer into hex after converting it to Zarith format.
    * @param {number} value Number to be obfuscated.
    */
-  export function writeInt(value: number) {
+  export function writeInt(value: number): string {
     //@ts-ignore
     return Buffer.from(base128.encode(parseInt(value)), "hex")
       .map((v, i) => {
@@ -24,7 +24,7 @@ export namespace TezosMessageUtils {
    * Takes a bounded hex string that is known to contain a number and decodes the int.
    * @param {string} hex Encoded message part.
    */
-  export function readInt(hex: string) {
+  export function readInt(hex: string): number {
     return base128.decode(
       //@ts-ignore
       Buffer.from(hex, "hex")
@@ -64,7 +64,7 @@ export namespace TezosMessageUtils {
    * @param {string} hex Encoded message part.
    * @param {string} type Tezos address type, one of 'tz1' or 'kt1'.
    */
-  export function readAddress(hex: string, type: string = "tz1") {
+  export function readAddress(hex: string, type: string = "tz1"): string {
     if (hex.length !== 40) { throw new Error("Incorrect hex length to parse an address."); }
 
     return type === "tz1" ? base58check.encode(Buffer.from("06a19f" + hex, "hex")) : base58check.encode(Buffer.from("025a79" + hex, "hex"));
@@ -73,19 +73,17 @@ export namespace TezosMessageUtils {
   /**
    * Encodes a Tezos address to hex, stripping off the top 3 bytes which contain address type, either 'tz1' or 'kt1'. Message format contains hints on address type.
    * @param {string} address Base58 address to encode.
+   * @returns {string} Hex represntaton of a Tezos address.
    */
-  export function writeAddress(address: string) {
-    return base58check
-      .decode(address)
-      .slice(3)
-      .toString("hex");
+  export function writeAddress(address: string): string {
+    return base58check.decode(address).slice(3).toString("hex");
   }
 
   /**
    * Reads the branch hash from the provided, bounded hex string.
    * @param {string} hex Encoded message part.
    */
-  export function readBranch(hex: string) {
+  export function readBranch(hex: string): string {
     if (hex.length !== 64) { throw new Error("Incorrect hex length to parse a branch hash."); }
     return base58check.encode(Buffer.from(hex, "hex"));
   }
@@ -93,16 +91,18 @@ export namespace TezosMessageUtils {
   /**
    * Encodes the branch hash to hex.
    * @param {string} branch Branch hash.
+   * @returns {string} Hex represntaton of the Base58 branch hash.
    */
-  export function writeBranch(branch: string) {
+  export function writeBranch(branch: string): string {
     return base58check.decode(branch).toString("hex");
   }
 
   /**
    * Reads the key hash from the provided, bounded hex string.
    * @param {string} hex Encoded message part.
+   * @returns {string} Key.
    */
-  export function readKey(hex: string) {
+  export function readKey(hex: string): string {
     if (hex.length !== 64) { throw new Error("Incorrect hex length to parse a key."); }
     return base58check.encode(Buffer.from(hex, "hex"));
   }
