0762e825e246cbdb67a672ed7b6b8a24c1c0dc15
ubasar <umurb@dcntrlzd.io>
2019-01-15 16:40:46 -0500

    fixed pubkey for alpha/mainnet

--- src/tezos/TezosOperations.ts
+++ src/tezos/TezosOperations.ts
@@ -305,8 +305,8 @@ export namespace TezosOperations {
             counter: (Number(account.counter) + 1).toString(),
             gas_limit: '10160',
             storage_limit: '277',
-            //managerPubkey: keyStore.publicKeyHash, // mainnet, alphanet
-            manager_pubkey: keyStore.publicKeyHash,  // zeronet
+            managerPubkey: keyStore.publicKeyHash, // mainnet, alphanet
+            //manager_pubkey: keyStore.publicKeyHash,  // zeronet
             balance: amount.toString(),
             spendable: spendable,
             delegatable: delegatable,
