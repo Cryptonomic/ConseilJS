d430dfac695b519d6931af45746a55e8a9c32cba
anonymoussprocket <anonymoussprocket@gmail.com>
2019-01-16 10:56:26 -0500

    - tests

--- test/TezosMessageCodec.spec.ts
+++ test/TezosMessageCodec.spec.ts
@@ -2,8 +2,8 @@ import { expect } from "chai";
 import { TezosMessageCodec } from "../src/tezos/TezosMessageCodec";
 import "mocha";
 
-describe("signOperationGroup() and computeOperationHash()", () => {
-  it("correctly parse a transaction from binary", () => {
+describe("Tezos P2P message decoder test suite", () => {
+  it("correctly parse a transaction", () => {
     let forgedTransaction = "9cd3aa823667cd8223073d2447c4c012373c99a00df5112b502ee82f5cf95ae408000069ef8fb5d47d8a4321c94576a2316a632be8ce898094ebdc039cfe19bc50ac0280a0c21e00004783ca75ac8736021036aa5ff2a69f116eca759800";
     const result = TezosMessageCodec.parseTransaction(forgedTransaction);
     expect(result.operation.kind).to.equal("transaction");
@@ -15,7 +15,7 @@ describe("signOperationGroup() and computeOperationHash()", () => {
     expect(result.operation.storage_limit).to.equal('300'); // microtez
   });
 
-  it("correctly parse a reveal from binary", () => {
+  it("correctly parse a reveal", () => {
     let forgedReveal = "97648f6470b21f904cb8d11eaf097f245eb42f5073fa51404d969cdfd4a4579e07000069ef8fb5d47d8a4321c94576a2316a632be8ce890094fe19904e00004c7b0501f6ea08f472b7e88791d3b8da49d64ac1e2c90f93c27e6531473305c6";
     const result = TezosMessageCodec.parseReveal(forgedReveal);
     expect(result.operation.kind).to.equal("reveal");
@@ -25,7 +25,7 @@ describe("signOperationGroup() and computeOperationHash()", () => {
     expect(result.operation.storage_limit).to.equal('0'); // microtez
   });
 
-  it("correctly parse an operation group from binary", () => {
+  it("correctly parse a reveal/transaction operation group", () => {
     let forgedGroup = "25827bef27daa0f8f0b6709bef72bbbcc76864fcaca9f892c77b16942f848acb08000069ef8fb5d47d8a4321c94576a2316a632be8ce898080897a9dfe19bc50ac028080c8d00701e7e3aecd3f15d831dee12179fc8063ccfe781e1f0000";
     const result = TezosMessageCodec.parseOperationGroup(forgedGroup);
     expect(result[0].kind).to.equal("transaction");
@@ -37,7 +37,7 @@ describe("signOperationGroup() and computeOperationHash()", () => {
     expect(result[0].storage_limit).to.equal('300'); // microtez
   });
 
-  it("correctly parse an origination from binary", () => {
+  it("correctly parse an origination", () => {
     let forgedOrigination = "30d0f6eceb58ee25a7477b8f6baa6dec852c038fb550a8c4a0f9016f4543f6c7090000b2e1d673031ec0711eacb822cbca4ce95f1e3c0a80897a8a05b04f950200b2e1d673031ec0711eacb822cbca4ce95f1e3c0a80dac409ffffff00641d2f258a7fafe9cf1f18720a14dfadba9adb0e00bf7366f8a48e82b025c898ba8eb7c333f1c720ccab3df932409c283b42c272e0b9cce73a4c657e0b9901726c8436fc2c177ba06f712dfd35320f67ee4b4dcf04";
     const result = TezosMessageCodec.parseOrigination(forgedOrigination);
     expect(result.operation.kind).to.equal("origination");
@@ -53,13 +53,13 @@ describe("signOperationGroup() and computeOperationHash()", () => {
     expect(result.operation.counter).to.equal('650');
   });
 
-  it("correctly parse an origination with contract from binary", () => {
+  it("correctly parse an origination with contract", () => {
     let forgedOrigination = "0f9c939d51f90e9435fe2f466058eed68cd7f0624ff439136dfe2dcc3139391c090000b2e1d673031ec0711eacb822cbca4ce95f1e3c0ac09a0c890581d901b04f00b2e1d673031ec0711eacb822cbca4ce95f1e3c0a80897affffff00641d2f258a7fafe9cf1f18720a14dfadba9adb0eff0000001c02000000170500036805010368050202000000080316053d036d03420000000a010000000568656c6c6f15f3cff9210c5287e91a2a9ba65871ed83b7c784ecf42419aa03c7bef02e50cd7916e228f15418ca74b7bb12d42f64cd9dac7e978173ff1360ff4dc7ca7bb501";
     const result = TezosMessageCodec.parseOrigination(forgedOrigination);
-    console.log(result.operation);
     expect(result.operation.kind).to.equal("origination");
     expect(result.operation.source).to.equal("tz1bwsWk3boyGgXf3u7CJGZSTfe14djdRtxG");
     expect(result.operation.managerPubkey).to.equal("tz1bwsWk3boyGgXf3u7CJGZSTfe14djdRtxG");
+    //script
     expect(result.operation.balance).to.equal('2000000'); // microtez
     expect(result.operation.spendable).to.equal(true);
     expect(result.operation.delegatable).to.equal(true);
