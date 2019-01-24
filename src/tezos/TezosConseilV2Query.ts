b6dd41765a965df2ab2370d33db05d56fe7fb800
Vishakh <vishakh@users.noreply.github.com>
2019-01-24 01:45:30 -0500

    Code cleanup.

--- src/tezos/TezosConseilV2Query.ts
+++ src/tezos/TezosConseilV2Query.ts
@@ -6,37 +6,37 @@ import {ConseilV2Filter, ConseilV2Operator, ConseilV2Query, ConseilV2SortDirecti
 
 export namespace TezosConseilV2Query {
 
-    const TEZOS = 'tezos'
-    const BLOCKS = 'blocks'
-    const ACCOUNTS = 'accounts'
-    const OPERATION_GROUPS = 'operation_groups'
-    const OPERATIONS = 'operations'
+    const TEZOS = 'tezos';
+    const BLOCKS = 'blocks';
+    const ACCOUNTS = 'accounts';
+    const OPERATION_GROUPS = 'operation_groups';
+    const OPERATIONS = 'operations';
 
     export async function getBlockHead(server: string, apiKey: string, network: string): Promise<object> {
-        var filter = ConseilV2Query.getEmptyFilter()
-        filter = ConseilV2Query.setOrderingForFilter(filter, 'level', ConseilV2SortDirection.DESC)
-        filter = ConseilV2Query.setLimitForFilter(filter, 1)
+        let filter = ConseilV2Query.getEmptyFilter();
+        filter = ConseilV2Query.setOrderingForFilter(filter, 'level', ConseilV2SortDirection.DESC);
+        filter = ConseilV2Query.setLimitForFilter(filter, 1);
         return ConseilV2Query.runDataQuery(apiKey, server, TEZOS, network, BLOCKS , filter)
     }
 
     export async function getBlock(server: string, apiKey: string, network: string, hash: string): Promise<object> {
-        var filter = ConseilV2Query.getEmptyFilter()
-        filter = ConseilV2Query.addPredicateToFilter(filter, 'hash', ConseilV2Operator.EQ, [hash], false)
-        filter = ConseilV2Query.setLimitForFilter(filter, 1)
+        let filter = ConseilV2Query.getEmptyFilter();
+        filter = ConseilV2Query.addPredicateToFilter(filter, 'hash', ConseilV2Operator.EQ, [hash], false);
+        filter = ConseilV2Query.setLimitForFilter(filter, 1);
         return ConseilV2Query.runDataQuery(apiKey, server, TEZOS, network, BLOCKS , filter)
     }
 
     export async function getAccount(server: string, apiKey: string, network: string, accountID: string): Promise<object> {
-        var filter = ConseilV2Query.getEmptyFilter()
-        filter = ConseilV2Query.addPredicateToFilter(filter, 'account_id', ConseilV2Operator.EQ, [accountID], false)
-        filter = ConseilV2Query.setLimitForFilter(filter, 1)
+        let filter = ConseilV2Query.getEmptyFilter();
+        filter = ConseilV2Query.addPredicateToFilter(filter, 'account_id', ConseilV2Operator.EQ, [accountID], false);
+        filter = ConseilV2Query.setLimitForFilter(filter, 1);
         return ConseilV2Query.runDataQuery(apiKey, server, TEZOS, network, ACCOUNTS , filter)
     }
 
     export async function getOperationGroup(server: string, apiKey: string, network: string, operationGroupID: string): Promise<object> {
-        var filter = ConseilV2Query.getEmptyFilter()
-        filter = ConseilV2Query.addPredicateToFilter(filter, 'operation_id', ConseilV2Operator.EQ, [operationGroupID], false)
-        filter = ConseilV2Query.setLimitForFilter(filter, 1)
+        let filter = ConseilV2Query.getEmptyFilter();
+        filter = ConseilV2Query.addPredicateToFilter(filter, 'operation_id', ConseilV2Operator.EQ, [operationGroupID], false);
+        filter = ConseilV2Query.setLimitForFilter(filter, 1);
         return ConseilV2Query.runDataQuery(apiKey, server, TEZOS, network, OPERATION_GROUPS , filter)
     }
 
