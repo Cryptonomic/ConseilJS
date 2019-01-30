7186f026c273592617a3e87284f30f9390e77e7e
ubasar <umurb@dcntrlzd.io>
2019-01-29 23:07:46 -0500

    removed dist

--- dist/types/Error.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Represents a generic cryptocurrency wallet.
- */
-export interface Error {
-    error: string;
-}
--- dist/types/HardwareDeviceType.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Enum describing the hardware device a Tezos Operation is acting upon
- */
-export declare enum HardwareDeviceType {
-    Ledger = 0
-}
