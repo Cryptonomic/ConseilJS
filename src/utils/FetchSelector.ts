3cab87e240bb0be60add03b49c8d709c81ba0760
anonymoussprocket <anonymoussprocket@gmail.com>
2019-02-08 14:50:27 -0500

    - doc

--- src/utils/FetchSelector.ts
+++ src/utils/FetchSelector.ts
@@ -1,5 +1,8 @@
 /**
- * Node/web compatibility for fetch.
+ * fetch interface abstraction.
+ * 
+ * nodejs and web-based applications building on ConseilJS will likely have different
+ * implementations of fetch available. This code allows ConseilJS use the appropriate version.
  */
 export default class FetchSelector {
     static fetch: any = null;
