ea98494a8c4908c6286692838178df5c4fcb2533
anonymoussprocket <anonymoussprocket@gmail.com>
2019-02-11 21:34:58 -0500

    - dist update

--- dist/utils/CryptoUtils.js
+++ dist/utils/CryptoUtils.js
@@ -10,14 +10,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const sodium = __importStar(require("sodium-native"));
+const sodium = __importStar(require("libsodium-wrappers"));
+const sodiumsumo = __importStar(require("libsodium-wrappers-sumo"));
+const crypto = __importStar(require("crypto"));
 const zxcvbn_1 = __importDefault(require("zxcvbn"));
 var CryptoUtils;
 (function (CryptoUtils) {
     function generateSaltForPwHash() {
-        let rand = Buffer.alloc(sodium.crypto_pwhash_SALTBYTES);
-        sodium.randombytes_buf(rand);
-        return rand;
+        return crypto.randomBytes(sodiumsumo.crypto_pwhash_SALTBYTES);
     }
     CryptoUtils.generateSaltForPwHash = generateSaltForPwHash;
     function encryptMessage(message, passphrase, salt) {
@@ -25,32 +25,25 @@ var CryptoUtils;
         if (passwordStrength < 3) {
             throw new Error('The password strength should not be less than 3.');
         }
-        const pwhash = Buffer.alloc(sodium.crypto_box_SEEDBYTES);
-        sodium.crypto_pwhash(pwhash, Buffer.from(passphrase), salt, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE, sodium.crypto_pwhash_ALG_DEFAULT);
-        let nonce = Buffer.alloc(sodium.crypto_box_NONCEBYTES);
-        sodium.randombytes_buf(nonce);
-        let ciphertext = Buffer.alloc(message.length + sodium.crypto_secretbox_MACBYTES);
-        sodium.crypto_secretbox_easy(ciphertext, Buffer.from(message), nonce, pwhash);
-        return Buffer.concat([nonce, ciphertext]);
+        const messageBytes = sodiumsumo.from_string(message);
+        const keyBytes = sodiumsumo.crypto_pwhash(sodiumsumo.crypto_box_SEEDBYTES, passphrase, salt, sodiumsumo.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodiumsumo.crypto_pwhash_MEMLIMIT_INTERACTIVE, sodiumsumo.crypto_pwhash_ALG_DEFAULT);
+        const nonce = Buffer.from(sodiumsumo.randombytes_buf(sodiumsumo.crypto_box_NONCEBYTES));
+        const cipherText = Buffer.from(sodiumsumo.crypto_secretbox_easy(messageBytes, nonce, keyBytes));
+        return Buffer.concat([nonce, cipherText]);
     }
     CryptoUtils.encryptMessage = encryptMessage;
     function decryptMessage(nonce_and_ciphertext, passphrase, salt) {
-        if (nonce_and_ciphertext.length < sodium.crypto_secretbox_NONCEBYTES + sodium.crypto_secretbox_MACBYTES) {
+        const keyBytes = sodiumsumo.crypto_pwhash(sodiumsumo.crypto_box_SEEDBYTES, passphrase, salt, sodiumsumo.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodiumsumo.crypto_pwhash_MEMLIMIT_INTERACTIVE, sodiumsumo.crypto_pwhash_ALG_DEFAULT);
+        if (nonce_and_ciphertext.length < sodiumsumo.crypto_secretbox_NONCEBYTES + sodiumsumo.crypto_secretbox_MACBYTES) {
             throw new Error("The cipher text is of insufficient length");
         }
-        const pwhash = Buffer.alloc(sodium.crypto_box_SEEDBYTES);
-        sodium.crypto_pwhash(pwhash, Buffer.from(passphrase), salt, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE, sodium.crypto_pwhash_ALG_DEFAULT);
-        const nonce = nonce_and_ciphertext.slice(0, sodium.crypto_secretbox_NONCEBYTES);
-        const ciphertext = nonce_and_ciphertext.slice(sodium.crypto_secretbox_NONCEBYTES);
-        let message = Buffer.alloc(ciphertext.length - sodium.crypto_secretbox_MACBYTES);
-        sodium.crypto_secretbox_open_easy(message, ciphertext, nonce, pwhash);
-        return message.toString();
+        const nonce = nonce_and_ciphertext.slice(0, sodiumsumo.crypto_secretbox_NONCEBYTES);
+        const ciphertext = nonce_and_ciphertext.slice(sodiumsumo.crypto_secretbox_NONCEBYTES);
+        return sodiumsumo.crypto_secretbox_open_easy(ciphertext, nonce, keyBytes, 'text');
     }
     CryptoUtils.decryptMessage = decryptMessage;
     function simpleHash(payload, length) {
-        let hash = Buffer.alloc(length);
-        sodium.crypto_generichash(hash, payload);
-        return hash;
+        return sodiumsumo.crypto_generichash(length, payload);
     }
     CryptoUtils.simpleHash = simpleHash;
     function getPasswordStrength(password) {
@@ -59,16 +52,12 @@ var CryptoUtils;
     }
     CryptoUtils.getPasswordStrength = getPasswordStrength;
     function generateKeys(seed) {
-        let secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES);
-        let publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES);
-        sodium.crypto_sign_seed_keypair(publicKey, secretKey, seed);
-        return { privateKey: secretKey, publicKey: publicKey };
+        const key_pair = sodiumsumo.crypto_sign_seed_keypair(seed, '');
+        return { privateKey: key_pair.privateKey, publicKey: key_pair.publicKey };
     }
     CryptoUtils.generateKeys = generateKeys;
-    function signDetached(payload, secretKey) {
-        let sig = Buffer.alloc(sodium.crypto_sign_BYTES);
-        sodium.crypto_sign_detached(sig, payload, secretKey);
-        return sig;
+    function signDetached(payload, privateKey) {
+        return sodium.crypto_sign_detached(payload, privateKey);
     }
     CryptoUtils.signDetached = signDetached;
 })(CryptoUtils = exports.CryptoUtils || (exports.CryptoUtils = {}));
