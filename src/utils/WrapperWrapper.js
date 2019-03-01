8f2bfee52db72e504f4e4e17f0cb28aa7022b809
anonymoussprocket <anonymoussprocket@gmail.com>
2019-03-01 16:21:42 -0500

    - var->const

--- src/utils/WrapperWrapper.js
+++ src/utils/WrapperWrapper.js
@@ -1,34 +1,34 @@
 const sodiumsumo = require('libsodium-wrappers-sumo');
 
-var rand = async (length) => {
+const rand = async (length) => {
     await sodiumsumo.ready;
     return sodiumsumo.randombytes_buf(length);
 }
 
-var salt = async () => {
+const salt = async () => {
     return rand(sodiumsumo.crypto_pwhash_SALTBYTES);
 }
 
-var nonce = async () => {
+const nonce = async () => {
     return rand(sodiumsumo.crypto_box_NONCEBYTES);
 }
 
-var keys = async (seed) => {
+const keys = async (seed) => {
     await sodiumsumo.ready;
     return sodiumsumo.crypto_sign_seed_keypair(seed, '');
 }
 
-var pwhash = async (passphrase, salt) => {
+const pwhash = async (passphrase, salt) => {
     await sodiumsumo.ready;
     return sodiumsumo.crypto_pwhash(sodiumsumo.crypto_box_SEEDBYTES, passphrase, salt, sodiumsumo.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodiumsumo.crypto_pwhash_MEMLIMIT_INTERACTIVE, sodiumsumo.crypto_pwhash_ALG_DEFAULT);
 }
 
-var close = async (message, nonce, key) => {
+const close = async (message, nonce, key) => {
     await sodiumsumo.ready;
     return sodiumsumo.crypto_secretbox_easy(message, nonce, key);
 }
 
-var open = async (nonce_and_ciphertext, key) => {
+const open = async (nonce_and_ciphertext, key) => {
     await sodiumsumo.ready;
 
     if (nonce_and_ciphertext.length < sodiumsumo.crypto_secretbox_NONCEBYTES + sodiumsumo.crypto_secretbox_MACBYTES) { throw new Error('The cipher text is of insufficient length'); }
@@ -39,7 +39,7 @@ var open = async (nonce_and_ciphertext, key) => {
     return sodiumsumo.crypto_secretbox_open_easy(ciphertext, nonce, key);
 }
 
-var sign = async (message, key) => {
+const sign = async (message, key) => {
     await sodiumsumo.ready;
     return sodiumsumo.crypto_sign_detached(message, key);
 }
