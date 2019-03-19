7afbd15dd65b57aae673385fc3060b0fe9f63dcf
anonymoussprocket <anonymoussprocket@gmail.com>
2019-03-19 12:54:55 -0400

    - tests

--- test/MichelineParser.spec.ts
+++ test/MichelineParser.spec.ts
@@ -74,6 +74,6 @@ describe('Micheline binary encoding tests', () => {
 describe('Micheline binary encoding complex tests', () => {
     it('test 0', () => {
         const result = michelsonToJson('[ { "prim": "parameter", "args": [ { "prim": "int" } ] }, { "prim": "storage", "args": [ { "prim": "int" } ] }, { "prim": "code", "args": [ [ { "prim": "CAR" }, { "prim": "PUSH", "args": [ { "prim": "int" }, { "int": "1" } ] }, { "prim": "ADD" }, { "prim": "NIL", "args": [ { "prim": "operation" } ] }, { "prim": "PAIR" } ] ] } ]');
-        expect(result).to.equal('020000001f0500035b0501035b0502020000001003160743035b00010312053d036d0342');
+        expect(result).to.equal('02000000030500035b0501035b0502020000000503160743035b00010312053d036d0342');
     });
 });
