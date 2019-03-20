0d5f670367d435117b41dfc67b028e8de2b7feea
anonymoussprocket <anonymoussprocket@gmail.com>
2019-03-20 09:59:26 -0400

    - automated contract tests

--- test/chain/tezos/lexer/MichelsonParser.spec.ts
+++ test/chain/tezos/lexer/MichelsonParser.spec.ts
@@ -11,15 +11,20 @@ function michelsonToMicheline(code: string): string {
     return parser.results.join(' ');
 }
 
-describe('Michelson Micheline transpiler tests', () => {
-    it('Contract test 0: src/bin_client/test/contracts/attic/add1.tz', async () => {
-        const code = await request.get('https://gitlab.com/nomadic-labs/tezos/raw/master/src/bin_client/test/contracts/attic/add1.tz')
-        .then(
-            res => res.toString()
-        ).catch(err => {
-            throw new Error(`Failed to get add1.tz: ${err}`);
-        });
+const baseURL = 'https://gitlab.com/tezos/tezos/raw/master/src/bin_client/test/contracts/attic';
+const officialContractSamples = ['accounts.tz', 'add1.tz', 'add1_list.tz'];
+const michelineTranslations = {'accounts.tz': '', 'add1.tz': '', 'add1_list.tz': ''};
 
-        expect(michelsonToMicheline(code)).to.equal('blah');
-    });
+describe('Michelson/Micheline transpiler tests', () => {
+    for (let i = 0; i < officialContractSamples.length; i++) {
+        const contractName = officialContractSamples[i];
+        it(`Contract test: ${contractName}`, async () => {
+            const code = await request.get(`${baseURL}/${contractName}`)
+            .then(res => res.toString())
+            .catch(err => { throw new Error(`Failed to get ${contractName}: ${err}`); });
+
+            expect(michelsonToMicheline(code)).to.equal(michelineTranslations[contractName]);
+        });
+    }
 });
+ 
