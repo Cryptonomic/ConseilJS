18a6f9f65d8cb4d85019ec6ae22607cb2ac62f21
anonymoussprocket <anonymoussprocket@gmail.com>
2019-06-13 20:35:17 -0400

    - increase delegation gas

--- dist/chain/tezos/TezosNodeWriter.js
+++ dist/chain/tezos/TezosNodeWriter.js
@@ -170,7 +170,7 @@ var TezosNodeWriter;
                 fee: fee.toString(),
                 counter: (Number(account.counter) + 1).toString(),
                 storage_limit: '0',
-                gas_limit: '10000',
+                gas_limit: '10600',
                 delegate: delegate
             };
             const operations = yield appendRevealOperation(network, keyStore, account, [delegation]);
