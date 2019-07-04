df5fbc65cf9a46473efd77503e5f4d0f31f07c98
anonymoussprocket <anonymoussprocket@gmail.com>
2019-07-03 22:55:24 -0400

    - improved type definitions for P2P operation messages - improved type definitions for RPC responses

---
+++ src/types/tezos/TezosP2PMessageTypes.ts
@@ -0,0 +1,81 @@
+/**
+ * Type definitions for messages that can be forged with the RPC service
+ */
+
+export interface Activation {
+    kind: string; // activate_account
+    pkh: string;
+    secret: string;
+}
+
+export interface Ballot {
+    source: string,
+    period: number,
+    proposal: string,
+    vote: BallotVote
+}
+
+export enum BallotVote {
+    Yay = 0,
+    Nay = 1,
+    Pass = 2
+}
+
+export interface Transaction {
+    kind: string,
+    source: string,
+    fee: string,
+    counter: string,
+    gas_limit: string,
+    storage_limit: string,
+    amount: string,
+    destination: string,
+    parameters?: string; // TODO: move to ContractInvocation
+}
+
+export interface Delegation {
+    kind: string,
+    source: string,
+    fee: string,
+    counter: string,
+    gas_limit: string,
+    storage_limit: string,
+    delegate?: string;
+}
+
+export interface Reveal {
+    kind: string;
+    source: string;
+    fee: string;
+    counter: string;
+    gas_limit: string;
+    storage_limit: string;
+    public_key: string;
+}
+
+export interface Origination {
+    kind: string;
+    source: string;
+    fee: string;
+    counter: string;
+    gas_limit: string;
+    storage_limit: string;
+    manager_pubkey: string;
+    balance: string;
+    spendable?: boolean;
+    delegatable?: boolean;
+    delegate?: string;
+    script?: object; // TODO: ContractOrigination
+}
+
+export interface ContractOrigination extends Origination {
+    script: object;
+}
+
+export interface ContractInvocation extends Transaction {
+    parameters: string;
+}
+
+export type Operation = Activation | Transaction | ContractInvocation | Delegation | Reveal | Origination | ContractOrigination;
+
+export type StackableOperation =  Transaction | ContractInvocation | Delegation | Origination | ContractOrigination;
