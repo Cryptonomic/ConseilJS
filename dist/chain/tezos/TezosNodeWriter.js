6f8066326c008684bb89f9b9be0e4edd812a6042
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-07-13 03:48:17 -0400

    Use MichelsonParameters parser on initial storage during deployment

--- dist/chain/tezos/TezosNodeWriter.js
+++ dist/chain/tezos/TezosNodeWriter.js
@@ -246,7 +246,7 @@ var TezosNodeWriter;
             if (codeFormat === TezosTypes.TezosParameterFormat.Michelson) {
                 parsedCode = JSON.parse(TezosLanguageUtil_1.TezosLanguageUtil.translateMichelsonToMicheline(code));
                 log.debug(`TezosNodeWriter.sendOriginationOperation code translation:\n${code}\n->\n${JSON.stringify(parsedCode)}`);
-                parsedStorage = JSON.parse(TezosLanguageUtil_1.TezosLanguageUtil.translateMichelsonToMicheline(storage));
+                parsedStorage = JSON.parse(TezosLanguageUtil_1.TezosLanguageUtil.translateMichelsonParametersToMicheline(storage));
                 log.debug(`TezosNodeWriter.sendOriginationOperation storage translation:\n${storage}\n->\n${JSON.stringify(parsedStorage)}`);
             }
             else if (codeFormat === TezosTypes.TezosParameterFormat.Micheline) {
