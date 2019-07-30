3a19902c247954ac7dffa66723132bd6d96a691d
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-07-30 16:49:33 -0400

    Add Tezos Baker Registry unit test for ContractIntrospector

--- integration_test/chain/tezos/TezosContractIntrospector.spec.ts
+++ integration_test/chain/tezos/TezosContractIntrospector.spec.ts
@@ -24,21 +24,29 @@ const keys: KeyStore = { // alphanet faucet account
 const ConseilV2URL = servers.conseilServer;
 const ConseilV2APIKey = servers.conseilApiKey;
 
-describe('TezosNodeReader integration test suite', () => {
-    it('Invoke a contract with a string literal parameter', async () => {
-        let result = await TezosContractIntrospector.generateEntryPointsFromAddress({ url: ConseilV2URL, apiKey: ConseilV2APIKey }, 'alphanet', 'KT1JfYDaNGwxVZB36SqeydVdBCVh6EVu93bP');
-        for (let entry of result) {
-            console.log(entry);
-            console.log();
-        }
-        expect(result[0]).to.equal()
+const conseilServer = {
+    url: ConseilV2URL,
+    apiKey: ConseilV2APIKey
+}
 
-        result = await TezosContractIntrospector.generateEntryPointsFromAddress({ url: ConseilV2URL, apiKey: ConseilV2APIKey }, 'alphanet', 'KT1XuBQ8FBEHSQU3jNQ8mwKpEado5djMdMtW');
-        for (let entry of result) {
-            console.log(entry);
-            console.log();
-        }
-        
-        //console.log(parserResult[0].generateParameter('food', 'drink', 'order'));
+describe('TezosContractIntrospector integration test suite', () => {
+    it('Generate entry points for Tezos Baker Registry (Alphanet)', async () => {
+        let result: EntryPoint[] = await TezosContractIntrospector.generateEntryPointsFromAddress(conseilServer, 'alphanet', 'KT1NpCh6tNQDmbmAVbGLxwRBx8jJD4rEFnmC');
+
+        expect(result[0].name).to.equal('%_Liq_entry_updateName');
+        expect(result[0].parameters.length).to.equal(1);
+        expect(result[0].generateParameter('"param1"')).to.equal('(Left "param1")');
+
+        expect(result[1].name).to.equal('%_Liq_entry_updatePaymentAddress');
+        expect(result[1].parameters.length).to.equal(1);
+        expect(result[1].generateParameter('"param1"')).to.equal('(Right (Left "param1"))');
+
+        expect(result[2].name).to.equal('%_Liq_entry_updateTerms');
+        expect(result[2].parameters.length).to.equal(3);
+        expect(result[2].generateParameter(1, 2, 3)).to.equal('(Right (Right (Left (Pair 1 (Pair 2 3)))))');
+
+        expect(result[3].name).to.equal('%_Liq_entry_deleteRegistration');
+        expect(result[3].parameters.length).to.equal(1);
+        expect(result[3].generateParameter('Unit')).to.equal('(Right (Right (Right Unit)))');
     });
 });
