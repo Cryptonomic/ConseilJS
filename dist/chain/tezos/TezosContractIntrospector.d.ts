c436c0d53c303be7b6366b36ffaea580ff55a755
anonymoussprocket <anonymoussprocket@gmail.com>
2019-08-01 01:30:35 -0400

    - remove dist:

--- dist/chain/tezos/TezosContractIntrospector.d.ts
+++
@@ -1,7 +0,0 @@
-import { TezosParameterFormat } from '../../types/tezos/TezosChainTypes';
-import { EntryPoint } from '../../types/tezos/ContractIntrospectionTypes';
-import { ConseilServerInfo } from '../../types/conseil/QueryTypes';
-export declare namespace TezosContractIntrospector {
-    function generateEntryPointsFromCode(contractCode: string, parameterFormat?: TezosParameterFormat): Promise<EntryPoint[]>;
-    function generateEntryPointsFromAddress(conseilServer: ConseilServerInfo, network: string, contractAddress: string): Promise<EntryPoint[]>;
-}
