e99eb05327ace688a3b83c6563994b93ba37ca16
Keefer Taylor <keefer@keefertaylor.com>
2020-11-24 21:59:50 -0700

    initial draft of token interactions

--- src/chain/tezos/contracts/TezosContractUtils.ts
+++ src/chain/tezos/contracts/TezosContractUtils.ts
@@ -7,19 +7,20 @@ import { TezosNodeReader } from '../TezosNodeReader';
  *
  */
 export namespace TezosContractUtils {
-/**
-     * Gets the contract code at the specified address at the head block and compares it to the known hash of the code. This function processes Micheline format contracts.
-     * 
-     * 
-     * @param server Destination Tezos node.
-     * @param address Contract address to query.
-     */
+    /**
+         * Gets the contract code at the specified address at the head block and compares it to the known hash of the code. This function processes Micheline format contracts.
+         * 
+         * 
+         * @param server Destination Tezos node.
+         * @param address Contract address to query.
+         */
     export async function verifyDestination(server: string, address: string, expected: string): Promise<boolean> {
         const contract = await TezosNodeReader.getAccountForBlock(server, 'head', address);
 
         if (!!!contract.script) { throw new Error(`No code found at ${address}`); }
 
         const k = Buffer.from(blakejs.blake2s(JSON.stringify(contract.script.code), null, 16)).toString('hex');
+        console.log("Expected: " + k)
 
         if (k !== expected) { throw new Error(`Contract code hash "${k}" doesn't match expected ${expected}`); }
 
