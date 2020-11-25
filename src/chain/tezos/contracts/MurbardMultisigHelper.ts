204d2a2b801c8f2a2c79044bef24abf06f9262f6
Keefer Taylor <keefer@keefertaylor.com>
2020-11-24 23:43:32 -0700

    revert mubarb

--- src/chain/tezos/contracts/MurbardMultisigHelper.ts
+++ src/chain/tezos/contracts/MurbardMultisigHelper.ts
@@ -8,55 +8,55 @@ import { KeyStore, Signer } from '../../../types/ExternalInterfaces';
 import * as TezosTypes from '../../../types/tezos/TezosChainTypes';
 
 export namespace MurbardMultisigHelper {
-  /**
-   * Gets the contract code at the specified address at the head block and compares it to the known hash of the code. This function processes Micheline format contracts.
-   * 
-   * @param server Destination Tezos node.
-   * @param address Contract address to query.
-   */
-  export async function verifyDestination(server: string, address: string): Promise<boolean> {
-    const contract = await TezosNodeReader.getAccountForBlock(server, 'head', address);
+    /**
+     * Gets the contract code at the specified address at the head block and compares it to the known hash of the code. This function processes Micheline format contracts.
+     * 
+     * @param server Destination Tezos node.
+     * @param address Contract address to query.
+     */
+    export async function verifyDestination(server: string, address: string): Promise<boolean> {
+        const contract = await TezosNodeReader.getAccountForBlock(server, 'head', address);
 
-    if (!!!contract.script) { throw new Error(`No code found at ${address}`); }
+        if (!!!contract.script) { throw new Error(`No code found at ${address}`); }
 
-    const k = Buffer.from(blakejs.blake2s(JSON.stringify(contract.script.code), null, 16)).toString('hex');
+        const k = Buffer.from(blakejs.blake2s(JSON.stringify(contract.script.code), null, 16)).toString('hex');
 
-    if (k !== '914629850cfdad7b54a8c5a661d10bd0') { throw new Error(`Contract does not match the expected code hash: ${k}, '914629850cfdad7b54a8c5a661d10bd0'`); }
+        if (k !== '914629850cfdad7b54a8c5a661d10bd0') { throw new Error(`Contract does not match the expected code hash: ${k}, '914629850cfdad7b54a8c5a661d10bd0'`); }
 
-    return true;
-  }
+        return true;
+    }
 
-  /**
-   * In contrast to verifyDestination, this function uses compares Michelson hashes.
-   * 
-   * @param script 
-   */
-  export function verifyScript(script: string): boolean {
-    const k = Buffer.from(blakejs.blake2s(TezosLanguageUtil.preProcessMichelsonScript(script).join('\n'), null, 16)).toString('hex');
+    /**
+     * In contrast to verifyDestination, this function uses compares Michelson hashes.
+     * 
+     * @param script 
+     */
+    export function verifyScript(script: string): boolean {
+        const k = Buffer.from(blakejs.blake2s(TezosLanguageUtil.preProcessMichelsonScript(script).join('\n'), null, 16)).toString('hex');
 
-    if (k !== 'ffcad1e376a6c8915780fe6676aceec6') { throw new Error(`Contract does not match the expected code hash: ${k}, 'ffcad1e376a6c8915780fe6676aceec6'`); }
+        if (k !== 'ffcad1e376a6c8915780fe6676aceec6') { throw new Error(`Contract does not match the expected code hash: ${k}, 'ffcad1e376a6c8915780fe6676aceec6'`); }
 
-    return true;
-  }
+        return true;
+    }
 
-  export async function getSimpleStorage(server: string, address: string): Promise<{ counter: number, threshold: number, keys: string[] }> {
-    const storageResult = await TezosNodeReader.getContractStorage(server, address);
+    export async function getSimpleStorage(server: string, address: string): Promise<{ counter: number, threshold: number, keys: string[] }> {
+        const storageResult = await TezosNodeReader.getContractStorage(server, address);
 
-    return {
-      counter: Number(JSONPath({ path: '$.args[0].int', json: storageResult })[0]),
-      threshold: Number(JSONPath({ path: '$.args[1].args[0].int', json: storageResult })[0]),
-      keys: JSONPath({ path: '$.args[1].args[1]..string', json: storageResult })
-    };
-  }
+        return {
+            counter: Number(JSONPath({ path: '$.args[0].int', json: storageResult })[0]),
+            threshold: Number(JSONPath({ path: '$.args[1].args[0].int', json: storageResult })[0]),
+            keys: JSONPath({ path: '$.args[1].args[1]..string', json: storageResult })
+        };
+    }
 
-  /**
-   * Sample multi-sig contract retrieved in April 2020.
-   * https://github.com/murbard/smart-contracts/blob/master/multisig/michelson/multisig.tzip
-   */
-  export async function deployContract(server: string, signer: Signer, keyStore: KeyStore, delegate: string, fee: number, amount: number, counter: number, threshold: number, keys: string[]): Promise<string> {
-    if (threshold > keys.length) { throw new Error('Number of keys provided is lower than the threshold'); }
+    /**
+     * Sample multi-sig contract retrieved in April 2020.
+     * https://github.com/murbard/smart-contracts/blob/master/multisig/michelson/multisig.tzip
+     */
+    export async function deployContract(server: string, signer: Signer, keyStore: KeyStore, delegate: string, fee: number, amount: number, counter: number, threshold: number, keys: string[]): Promise<string> {
+        if (threshold > keys.length) { throw new Error('Number of keys provided is lower than the threshold'); }
 
-    const code = `parameter (pair (pair :payload (nat %counter) (or :action (pair :transfer (mutez %amount) (contract %dest unit)) (or (option %delegate key_hash) (pair %change_keys (nat %threshold) (list %keys key))))) (list %sigs (option signature)));
+        const code = `parameter (pair (pair :payload (nat %counter) (or :action (pair :transfer (mutez %amount) (contract %dest unit)) (or (option %delegate key_hash) (pair %change_keys (nat %threshold) (list %keys key))))) (list %sigs (option signature)));
         storage (pair (nat %stored_counter) (pair (nat %threshold) (list %keys key)));
         code
           {
@@ -106,13 +106,13 @@ export namespace MurbardMultisigHelper {
                         {
                           DIP { SWAP ; CAR } ; SWAP ; PAIR ; SWAP }} ;
             PAIR }`;
-    const storage = `(Pair ${counter} (Pair ${threshold} { "${keys.join('" ; "')}" } ) )`;
+        const storage = `(Pair ${counter} (Pair ${threshold} { "${keys.join('" ; "') }" } ) )`;
 
-    const nodeResult = await TezosNodeWriter.sendContractOriginationOperation(server, signer, keyStore, amount, delegate, fee, 5_000, 120_000, code, storage, TezosTypes.TezosParameterFormat.Michelson);
-    return clearRPCOperationGroupHash(nodeResult['operationGroupID']);
-  }
+        const nodeResult = await TezosNodeWriter.sendContractOriginationOperation(server, signer, keyStore, amount, delegate, fee, 5_000, 120_000, code, storage, TezosTypes.TezosParameterFormat.Michelson);
+        return clearRPCOperationGroupHash(nodeResult['operationGroupID']);
+    }
 
-  function clearRPCOperationGroupHash(hash: string): string {
-    return hash.replace(/\"/g, '').replace(/\n/, '');
-  }
+    function clearRPCOperationGroupHash(hash: string): string {
+        return hash.replace(/\"/g, '').replace(/\n/, '');
+    }
 }
