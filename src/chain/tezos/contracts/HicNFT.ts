80372d4cdacb4c5145fd26b4f7d7a9e76946bf54
itamar <itamar.reif@gmail.com>
2021-05-28 20:35:30 +0300

    removed redundant code and included utils

--- src/chain/tezos/contracts/HicNFT.ts
+++ src/chain/tezos/contracts/HicNFT.ts
@@ -1,232 +1,251 @@
 import {JSONPath} from 'jsonpath-plus';
-
+import {BigNumber} from 'bignumber.js';
 import {KeyStore, Signer} from '../../../types/ExternalInterfaces';
 import {TezosNodeReader} from '../TezosNodeReader';
 import {TezosNodeWriter} from '../TezosNodeWriter';
 import {ConseilOperator, ConseilSortDirection, ConseilServerInfo} from '../../../types/conseil/QueryTypes';
 import {ConseilQueryBuilder} from '../../../reporting/ConseilQueryBuilder';
 import {TezosConseilClient} from '../../../reporting/tezos/TezosConseilClient';
-import {MultiAssetTokenHelper} from './tzip12/MultiAssetTokenHelper';
+import {MultiAssetTokenHelper, MultiAssetSimpleStorage, TransferPair} from './tzip12/MultiAssetTokenHelper';
 import {TezosParameterFormat} from '../../../types/tezos/TezosChainTypes';
 import {TezosContractUtils} from './TezosContractUtils';
+import {TezosMessageUtils} from '../TezosMessageUtil';
 
 export namespace HicNFTHelper {
-    /*
-     * Type for representing Hic's FA2 contracts' storage.
-     * @param administrator
-     * @param allTokens
-     * @param ledgerMapId
-     * @param metadataMap
-     * @param operatorsMapId
-     * @param paused
-     * @param tokenMetadataMapId
-     */
-    interface HicStorage {
-        administrator: string;
-        allTokens: number;
-        ledgerMapId: number;
-        metadataMapId: number;
-        operatorsMapId: number;
-        paused: boolean;
-        tokenMetadataMapId: number;
-    }
-
     export const objktsAddress = 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton';
     export const hDaoAddress = 'KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW';
 
     /*
-     * Objkts FA2 contract storage type
-     */
-    export type ObjktsStorage = HicStorage;
-
-    /*
      * Get an instance of a Objkts contract's storage by querying a given address
      * @param server The Tezos node to communicate with
-     * @param address The deployed Timelock contract address
+     * @param address Contract address, i.e. HicNFTHelper.objktsAddress or HicNFTHelper.hDaoAddress
      */
-    export async function getObjktsStorage(server: string): Promise<ObjktsStorage> {
-        const storageResult = await TezosNodeReader.getContractStorage(server, objktsAddress);
+    export async function getStorage(server: string, address: string): Promise<MultiAssetSimpleStorage> {
+        const storageResult = await TezosNodeReader.getContractStorage(server, address);
         return {
             administrator: JSONPath({path: '$.args[0].args[0].string', json: storageResult })[0],
-            allTokens: Number(JSONPath({ path: '$.args[0].args[1].int', json: storageResult })[0]),
-            ledgerMapId: Number(JSONPath({ path: '$.args[0].args[2].int', json: storageResult })[0]),
-            metadataMapId: Number(JSONPath({ path: '$.args[1].args[0].int', json: storageResult })[0]),
-            operatorsMapId: Number(JSONPath({ path: '$.args[1].args[1].int', json: storageResult })[0]),
+            tokens: Number(JSONPath({ path: '$.args[0].args[1].int', json: storageResult })[0]),
+            ledger: Number(JSONPath({ path: '$.args[0].args[2].int', json: storageResult })[0]),
+            metadata: Number(JSONPath({ path: '$.args[1].args[0].int', json: storageResult })[0]),
+            operators: Number(JSONPath({ path: '$.args[1].args[1].int', json: storageResult })[0]),
             paused: (JSONPath({ path: '$.args[2].prim', json: storageResult })[0]).toString().toLowerCase().startsWith('t'),
-            tokenMetadataMapId: Number(JSONPath({ path: '$.args[3].int', json: storageResult })[0])
+            tokenMetadata: Number(JSONPath({ path: '$.args[3].int', json: storageResult })[0])
         };
     }
 
     /*
-     * hDao FA2 contract storage type
+     * Returns a query for the last price.
+     *
+     * @param operations Array of chunks of operations (see `chunkArray()`)
      */
-    export type HDaoStorage = HicStorage;
+    function makeLastPriceQuery(operations) {
+        let lastPriceQuery = ConseilQueryBuilder.blankQuery();
+        lastPriceQuery = ConseilQueryBuilder.addFields(lastPriceQuery, 'timestamp', 'amount', 'operation_group_hash', 'parameters_entrypoints', 'parameters');
+        lastPriceQuery = ConseilQueryBuilder.addPredicate(lastPriceQuery, 'kind', ConseilOperator.EQ, ['transaction']);
+        lastPriceQuery = ConseilQueryBuilder.addPredicate(lastPriceQuery, 'status', ConseilOperator.EQ, ['applied']);
+        lastPriceQuery = ConseilQueryBuilder.addPredicate(lastPriceQuery, 'internal', ConseilOperator.EQ, ['false']);
+        lastPriceQuery = ConseilQueryBuilder.addPredicate(
+            lastPriceQuery,
+            'operation_group_hash',
+            operations.length > 1 ? ConseilOperator.IN : ConseilOperator.EQ,
+            operations
+        );
+        lastPriceQuery = ConseilQueryBuilder.setLimit(lastPriceQuery, operations.length);
 
-
-    /*
-     * Get an instance of an hDao contract's storage by querying a given address
-     * @param server The Tezos node to communicate with
-     * @param address The deployed Timelock contract address
-     */
-    export async function getHDaoStorage(server: string): Promise<HDaoStorage> {
-        const storageResult = await TezosNodeReader.getContractStorage(server, hDaoAddress);
-        return {
-            administrator: JSONPath({path: '$.args[0].args[0].string', json: storageResult })[0],
-            allTokens: Number(JSONPath({ path: '$.args[0].args[1].int', json: storageResult })[0]),
-            ledgerMapId: Number(JSONPath({ path: '$.args[0].args[2].int', json: storageResult })[0]),
-            metadataMapId: Number(JSONPath({ path: '$.args[1].args[0].int', json: storageResult })[0]),
-            operatorsMapId: Number(JSONPath({ path: '$.args[1].args[1].int', json: storageResult })[0]),
-            paused: (JSONPath({ path: '$.args[2].prim', json: storageResult })[0]).toString().toLowerCase().startsWith('t'),
-            tokenMetadataMapId: Number(JSONPath({ path: '$.args[3].int', json: storageResult })[0])
-        };
+        return lastPriceQuery;
     }
 
     /*
-     * FA2 individual token transfer
+     * Retreives the collection of tokens owned by managerAddress.
+     *
+     * @param tokenMapId
+     * @param managerAddress
+     * @param serverInfo
      */
-    interface TokenTransaction {
-        destination: string;
-        token_id: number;
-        amount: number;
-    }
+    export async function getCollection(tokenMapId: number, managerAddress: string, serverInfo: ConseilServerInfo): Promise<any[]> {
+        let collectionQuery = ConseilQueryBuilder.blankQuery();
+        // TODO: add comment with the query
+        collectionQuery = ConseilQueryBuilder.addFields(collectionQuery, 'key', 'value', 'operation_group_id');
+        collectionQuery = ConseilQueryBuilder.addPredicate(collectionQuery, 'big_map_id', ConseilOperator.EQ, [tokenMapId]);
+        collectionQuery = ConseilQueryBuilder.addPredicate(collectionQuery, 'key', ConseilOperator.STARTSWITH, [
+            `Pair 0x${TezosMessageUtils.writeAddress(managerAddress)}`,
+        ]);
+        collectionQuery = ConseilQueryBuilder.addPredicate(collectionQuery, 'value', ConseilOperator.EQ, [0], true);
+        collectionQuery = ConseilQueryBuilder.setLimit(collectionQuery, 10_000);
 
-    /*
-     * Returns a TokenTransaction in Michelson format
-     */
-    export function TokenTransactionMichelson(tx: TokenTransaction): string {
-        return `Pair "${tx.destination}" (Pair ${tx.token_id} ${tx.amount})`;
-    }
+        const collectionResult = await TezosConseilClient.getTezosEntityData(serverInfo, serverInfo.network, 'big_map_contents', collectionQuery);
 
-    /*
-     * FA2 batch transfer invocation parameters
-     */
-    interface TransferPair {
-        source: string;
-        txs: TokenTransaction[]
-    }
+        const operationGroupIds = collectionResult.map((r) => r.operation_group_id);
+        const queryChunks = chunkArray(operationGroupIds, 30);
+        const priceQueries = queryChunks.map((c) => makeLastPriceQuery(c));
 
-    /*
-     * Returns a TransferPair in Michelson format
-     */
-    export function TransferPairMichelson(transfers: TransferPair[]): string {
-        const transferList: string = transfers.map(
-            (transfer) => {
-                const txList: string = transfer.txs.map((tx) => TokenTransactionMichelson(tx)).join("; ");
-                return `Pair "${transfer.source}" { ${txList} }`;
+        const priceMap: any = {};
+        await Promise.all(
+            priceQueries.map(
+                async (q) =>
+                    await TezosConseilClient.getTezosEntityData(serverInfo, serverInfo.network, 'operations', q).then((result) =>
+                        result.map((row) => {
+                            let amount = 0;
+                            const action = row.parameters_entrypoints;
+
+                            if (action === 'collect') {
+                                amount = Number(row.parameters.toString().replace(/^Pair ([0-9]+) [0-9]+/, '$1'));
+                            } else if (action === 'transfer') {
+                                amount = Number(
+                                    row.parameters
+                                        .toString()
+                                        .replace(
+                                            /[{] Pair \"[1-9A-HJ-NP-Za-km-z]{36}\" [{] Pair \"[1-9A-HJ-NP-Za-km-z]{36}\" [(]Pair [0-9]+ [0-9]+[)] [}] [}]/,
+                                            '$1'
+                                        )
+                                );
+                            }
+
+                            priceMap[row.operation_group_hash] = {
+                                price: new BigNumber(row.amount),
+                                amount,
+                                timestamp: row.timestamp,
+                                action,
+                            };
+                        })
+                    )
+            )
+        );
+
+        const collection = collectionResult.map((row) => {
+            let price = 0;
+            let receivedOn = new Date();
+            let action = '';
+
+            try {
+                const priceRecord = priceMap[row.operation_group_id];
+                price = priceRecord.price.dividedToIntegerBy(priceRecord.amount).toNumber();
+                receivedOn = new Date(priceRecord.timestamp);
+                action = priceRecord.action === 'collect' ? 'Purchased' : 'Received';
+            } catch {
+                //
             }
-        ).join("; ");
-        return `{ ${transferList} }`
+
+            return {
+                piece: row.key.toString().replace(/.* ([0-9]{1,}$)/, '$1'),
+                amount: Number(row.value),
+                price: isNaN(price) ? 0 : price,
+                receivedOn,
+                action,
+            };
+        });
+
+        return collection.sort((a, b) => b.receivedOn.getTime() - a.receivedOn.getTime());
     }
 
     /*
-     * hDao transaction parameters
-     */
-    export type HDaoTransaction = TokenTransaction;
-
-    /*
-     * hDao transfer entrypoint parameters
-     */
-    export type HDaoTransferPair = TransferPair;
-
-    /*
-     * Helper function for single (i.e. non-batched) hDao FA2 transfer
+     * Fetch an account's collection and return its size.
      *
-     * @param src
-     * @param dest
-     * @param amount
+     * @param
      */
-    export function makeSingleHDaoTransfer(src: string, dest: string, amount: number): HDaoTransferPair {
-        return {
-            source: src,
-            txs: [ { destination: dest, token_id: 0, amount: amount } ]
-        }
+    export async function getCollectionSize(tokenMapId: number, managerAddress: string, serverInfo: ConseilServerInfo): Promise<number> {
+        const collection = await getCollection(tokenMapId, managerAddress, serverInfo);
+        const tokenCount = collection.reduce((a, c) => a + c.amount, 0);
+
+        return tokenCount;
     }
 
-    /*
-     * Invokes the Transfer entrypoint of the hDao FA2 contract.
+    /**
+     * Returns raw hDAO token balance for the account.
+     * KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW, ledger map id 515
      *
      * @param server
-     * @param signer
-     * @param keystore
+     * @param mapId
      * @param address
-     * @param parameter
-     * @param amount
-     * @param fee
-     * @param gas
-     * @param freight
      */
-    export async function hDaoTransfer(server: string, signer: Signer, keystore: KeyStore, transfers: HDaoTransferPair[], amount: number, fee: number, gas: number, freight: number): Promise<string> {
-        const entrypoint = `transfer`;
+    export async function getBalance(server: string, mapId: number, address: string): Promise<number> {
+        const packedTokenKey = TezosMessageUtils.encodeBigMapKey(
+            Buffer.from(TezosMessageUtils.writePackedData(`(Pair 0x${TezosMessageUtils.writeAddress(address)} 0)`, '', TezosParameterFormat.Michelson), 'hex')
+        );
+        let balance = 0;
 
-        let parameter: string = TransferPairMichelson(transfers);
-        const nodeResult = await TezosNodeWriter.sendContractInvocationOperation(
-            server,
-            signer,
-            keystore,
-            hDaoAddress, // 'KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW'
-            amount,
-            fee,
-            freight,
-            gas,
-            entrypoint,
-            parameter,
-            TezosParameterFormat.Michelson);
-        return TezosContractUtils.clearRPCOperationGroupHash(nodeResult.operationGroupID);
-    }
-
-    /*
-     * Objkts transaction parameters
-     */
-    export type ObjktsTransaction = TokenTransaction;
-
-    /*
-     * Objkts transfer entrypoint parameters
-     */
-    export type ObjktsTransferPair = TransferPair;
-
-    /*
-     * Helper function for single (i.e. non-batched) objkts FA2 transfer
-     *
-     * @param src
-     * @param dest
-     * @param amount
-     */
-    export function makeSingleObjktsTransfer(src: string, dest: string, token_id: number, amount: number): ObjktsTransferPair {
-        return {
-            source: src,
-            txs: [ { destination: dest, token_id: token_id, amount: amount } ]
+        try {
+            const balanceResult = await TezosNodeReader.getValueForBigMapKey(server, mapId, packedTokenKey);
+            balance = new BigNumber(JSONPath({ path: '$.int', json: balanceResult })[0]).toNumber();
+        } catch (err) {
+            //
         }
+
+        return balance;
     }
 
     /*
-     * Invokes the Transfer entrypoint of the hDao FA2 contract.
+     * Return the number of holders and total balance held of hDao tokens.
      *
-     * @param server
-     * @param signer
-     * @param keystore
-     * @param address
-     * @param parameter
-     * @param amount
-     * @param fee
-     * @param gas
-     * @param freight
+     * @param
      */
-    export async function objktsTransfer(server: string, signer: Signer, keystore: KeyStore, transfers: HDaoTransferPair[], amount: number, fee: number, gas: number, freight: number): Promise<string> {
-        const entrypoint = `transfer`;
+    export async function getTokenInfo(serverInfo: ConseilServerInfo, mapId: number = 515): Promise<{ holders: number; totalBalance: number }> {
+        let holdersQuery = ConseilQueryBuilder.blankQuery();
+        holdersQuery = ConseilQueryBuilder.addFields(holdersQuery, 'value');
+        holdersQuery = ConseilQueryBuilder.addPredicate(holdersQuery, 'big_map_id', ConseilOperator.EQ, [mapId]);
+        holdersQuery = ConseilQueryBuilder.setLimit(holdersQuery, 20_000);
 
-        let parameter: string = TransferPairMichelson(transfers);
-        const nodeResult = await TezosNodeWriter.sendContractInvocationOperation(
-            server,
-            signer,
-            keystore,
-            objktsAddress, // 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton'
-            amount,
-            fee,
-            freight,
-            gas,
-            entrypoint,
-            parameter,
-            TezosParameterFormat.Michelson);
-        return TezosContractUtils.clearRPCOperationGroupHash(nodeResult.operationGroupID);
+        const holdersResult = await TezosConseilClient.getTezosEntityData(serverInfo, serverInfo.network, 'big_map_contents', holdersQuery);
+
+        let holders = 0;
+        let totalBalance = new BigNumber(0);
+        holdersResult.forEach((r) => {
+            try {
+                const balance = new BigNumber(r.value);
+                if (balance.isGreaterThan(0)) {
+                    totalBalance = totalBalance.plus(balance);
+                }
+                holders++;
+            } catch {
+                // eh
+            }
+        });
+
+        return { holders, totalBalance: totalBalance.toNumber() };
+    }
+
+    /*
+     * Retrieves an NFT object's metadata.
+     *
+     * @param server The Teznos node to query
+     * @param objectId The token_id of the NFT to query
+     */
+    export async function getNFTObjectDetails(server: string, objectId: number) {
+        const packedNftId = TezosMessageUtils.encodeBigMapKey(Buffer.from(TezosMessageUtils.writePackedData(objectId, 'int'), 'hex'));
+        const nftInfo = await TezosNodeReader.getValueForBigMapKey(server, 514, packedNftId);
+        const ipfsUrlBytes = JSONPath({ path: '$.args[1][0].args[1].bytes', json: nftInfo })[0];
+        const ipfsHash = Buffer.from(ipfsUrlBytes, 'hex').toString().slice(7);
+
+        const nftDetails = await fetch(`https://cloudflare-ipfs.com/ipfs/${ipfsHash}`, { cache: 'no-store' });
+        const nftDetailJson = await nftDetails.json();
+
+        const nftName = nftDetailJson.name;
+        const nftDescription = nftDetailJson.description;
+        const nftCreators = nftDetailJson.creators
+            .map((c) => c.trim())
+            .map((c) => `${c.slice(0, 6)}...${c.slice(c.length - 6, c.length)}`)
+            .join(', '); // TODO: use names where possible
+        const nftArtifact = `https://cloudflare-ipfs.com/ipfs/${nftDetailJson.formats[0].uri.toString().slice(7)}`;
+        const nftArtifactType = nftDetailJson.formats[0].mimeType.toString();
+
+        return { name: nftName, description: nftDescription, creators: nftCreators, artifactUrl: nftArtifact, artifactType: nftArtifactType };
+    }
+
+    /*
+     * Turn an array of n=k*len elements into an array of k arrays of length len.
+     *
+     * @param arr
+     * @param len
+     */
+    function chunkArray(arr: any[], len: number) {
+        const chunks: any[] = [];
+        const n = arr.length;
+
+        let i = 0;
+        while (i < n) {
+            chunks.push(arr.slice(i, (i += len)));
+        }
+
+        return chunks;
     }
 }
--- src/chain/tezos/contracts/tzip12/MultiAssetTokenHelper.ts
+++ src/chain/tezos/contracts/tzip12/MultiAssetTokenHelper.ts
@@ -6,7 +6,7 @@ import { TezosNodeReader } from '../../TezosNodeReader';
 import { TezosNodeWriter } from '../../TezosNodeWriter';
 import { TezosContractUtils } from '../TezosContractUtils';
 
-interface MultiAssetSimpleStorage {
+export interface MultiAssetSimpleStorage {
     administrator: string,
     tokens: number,
     ledger: number,
@@ -14,21 +14,52 @@ interface MultiAssetSimpleStorage {
     paused: string,
     operators: number,
     tokenMetadata: number,
-    totalSupply: number
+    totalSupply?: number
 }
 
-interface MultiAssetTokenDefinition {
+export interface MultiAssetTokenDefinition {
     tokenid: number;
     metadata: Record<string, string>;
 }
 
-interface TransferPair {
-    address: string;
-    tokenid: number;
+/*
+ * FA2 individual token transfer
+ */
+export interface TokenTransfer {
+    destination: string;
+    token_id: number;
     amount: number;
 }
 
-interface UpdateOperator {
+/*
+ * Returns a TokenTransaction in Michelson format
+ */
+export function TokenTransactionMichelson(tx: TokenTransfer): string {
+    return `Pair "${tx.destination}" (Pair ${tx.token_id} ${tx.amount})`;
+}
+
+/*
+ * FA2 batch transfer invocation parameters
+ */
+export interface TransferPair {
+    source: string;
+    txs: TokenTransfer[]
+}
+
+/*
+ * Returns a TransferPair in Michelson format
+ */
+export function TransferPairMichelson(transfers: TransferPair[]): string {
+    const transferList: string = transfers.map(
+        (transfer) => {
+            const txList: string = transfer.txs.map((tx) => TokenTransactionMichelson(tx)).join("; ");
+            return `Pair "${transfer.source}" { ${txList} }`;
+        }
+    ).join("; ");
+    return `{ ${transferList} }`
+}
+
+export interface UpdateOperator {
     owner: string,
     operator: string,
     tokenid: number
@@ -195,9 +226,9 @@ export namespace MultiAssetTokenHelper {
         return TezosContractUtils.clearRPCOperationGroupHash(nodeResult.operationGroupID);
     }
 
-    export async function transfer(server: string, address: string, signer: Signer, keystore: KeyStore, fee: number, source: string, transfers: TransferPair[], gas: number = 800_000, freight: number = 20_000): Promise<string> {
+    export async function transfer(server: string, address: string, signer: Signer, keystore: KeyStore, fee: number, transfers: TransferPair[], gas: number = 800_000, freight: number = 20_000): Promise<string> {
         const entryPoint = 'transfer';
-        const parameters = `{ Pair "${source}" { ${transfers.map(t => '( Pair "' + t.address + '" ( Pair ' + t.tokenid + ' ' + t.amount + ' ) )').join(' ; ')} } }`;
+        const parameters = TransferPairMichelson(transfers);
 
         const nodeResult = await TezosNodeWriter.sendContractInvocationOperation(server, signer, keystore, address, 0, fee, freight, gas, entryPoint, parameters, TezosTypes.TezosParameterFormat.Michelson);
 
